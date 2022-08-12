72a647f3d43b9b6d7c869716d6dd404fa5e37e0d
quoimec <18281573+quoimec@users.noreply.github.com>
2022-08-13 06:37:58 +1000

    Initial content commit

---
+++ templates/register.js
@@ -0,0 +1,21 @@
+
+var templates = ["header"];
+
+console.log(document.getElementById("schacher-header"));
+
+// fetch("./index.html")
+//     .then(response => response.text())
+//     .then((data) => {
+//         console.log(data)
+//     });
+
+
+
+// class SchacherHeader extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = "";
+//     }
+// }
+
+// customElements.define('main-header', Header);
+// customElements.define('main-footer', Footer);
